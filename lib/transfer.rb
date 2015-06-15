6c938ef032a79073300ec27f19df623fefc04003
hpetrow <heather.petrow@gmail.com>
2015-06-15 10:38:58 -0400

    done

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,40 @@
+require 'pry'
+
 class Transfer
+  attr_reader :sender, :receiver, :amount
+  attr_accessor :status
+
   # code here
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+
+  def both_valid?
+    sender.valid? && receiver.valid?
+  end
+
+  def execute_transaction
+    sender_new_balance = sender.balance - amount
+    if sender_new_balance > 0
+      if @status != "complete"
+        sender.balance = sender_new_balance
+        receiver.balance += amount
+        @status = "complete"
+      end
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+      sender.balance += amount
+      receiver.balance -= amount
+      @status = "reversed"
+    end
+  end
 end
